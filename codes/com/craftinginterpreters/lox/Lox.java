8f7a6f5b9c47ecf446562f3cda871618c5ba1d6e
cire23097 <cire23097@g.nihon-u.ac.jp>
2025-12-16 12:00:26 +0900

    all file

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -42,14 +42,15 @@ public class Lox {
 
         private static void run(String source) {
             Scanner scanner = new Scanner(source);
-            List<Token> tokens = scanner.scanTokens();   
+            List<Token> tokens = scanner.scanTokens();
             Parser parser = new Parser(tokens);
             Expr expression = parser.parse();
 
-    // Stop if there was a syntax error.
-    if (hadError) return;
+            // Stop if there was a syntax error.
+            if (hadError) return;
 
-    System.out.println(new AstPrinter().print(expression));
+            System.out.println(new AstPrinter().print(expression));
+
 
             // For now, just print the tokens.
             for (Token token : tokens) {
@@ -65,7 +66,8 @@ public class Lox {
             System.err.println("[line " + line + "] Error" + where + ": " + message);
             hadError = true;
         }
-          static void error(Token token, String message) {
+
+        static void error(Token token, String message) {
     if (token.type == TokenType.EOF) {
       report(token.line, " at end", message);
     } else {
